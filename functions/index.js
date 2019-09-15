45007bda46eea57ba55567f7c062409d57e8926a
Julie Xiao <julixiao@gmail.com>
2019-09-15 03:58:11 -0400

    Submitting

--- functions/index.js
+++ functions/index.js
@@ -2,32 +2,18 @@
 const functions = require('firebase-functions');
 
 const express = require('express')
-
+// const firebase = require("firebase");
+// Required for side-effects
+// require("firebase/firestore");  
 const app = express();
 var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
-
-// var LIBRARY = LIBRARY || (function(){
-//     var _args = picked; // private
-//     return {
-//         transfer : function() {
-//             alert(_args);
-//         }
-//     };
-// });
 
 app.get('/', function (req, res) {
     res.sendFile('public/index.html');
        // Create the places service.
 });
 
-// app.post('/goEat', function (req, res) {
-//     //res.on('finish',filter());
-//     res.sendFile(__dirname + '/goEat.html');
-// });
-
 var server = app.listen(5000, function () {
     console.log('Node server is running..');
 });
-
-// exports.app = functions.https.onRequest(app);
